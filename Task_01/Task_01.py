a825c5effb91b9e9f3e76ea5c8ac58847348d2cc
MikleBuntin <BuntinMA@mail.ru>
2022-10-24 19:01:26 +0300

    Решил задачу 1.

--- Task_01/Task_01.py
+++ Task_01/Task_01.py
@@ -6,4 +6,15 @@ file.close()
 
 text_array = text.split()
 
-print(text_array)
+print("old: ", text)
+new_text_array = []
+for i in range(0, len(text_array)):
+    if text_array[i].find("абв") < 0:
+        new_text_array.append(text_array[i])
+
+
+new = str(new_text_array[0])
+for i in range(1, len(new_text_array)):
+    new = new + " " + str(new_text_array[i])
+
+print("new: ", new)
